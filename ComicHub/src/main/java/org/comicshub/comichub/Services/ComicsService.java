f48db5f14fb7ed9402228ba99139fd75acfeb2a6
NazaryiMontytskyi <111142609+NazaryiMontytskyi@users.noreply.github.com>
2024-12-06 02:15:12 +0200

    filtering and searching comics

--- ComicHub/src/main/java/org/comicshub/comichub/Services/ComicsService.java
+++ ComicHub/src/main/java/org/comicshub/comichub/Services/ComicsService.java
@@ -2,10 +2,14 @@ package org.comicshub.comichub.Services;
 
 
 import org.comicshub.comichub.Models.Comic;
+import org.comicshub.comichub.Models.Country;
+import org.comicshub.comichub.Models.Genre;
 import org.comicshub.comichub.Models.UserRead;
 import org.comicshub.comichub.Repositories.ComicsRepository;
 import org.comicshub.comichub.Security.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -56,6 +60,28 @@ public class ComicsService {
             favourites.add(this.comicsRepository.findById(userRead.getComic().getId()));
         }
         return favourites;
+    }
+
+    @Transactional
+    public Page<Comic> getFilteredComics(Genre genre, Country country, Pageable pageable){
+        if(genre != null && country != null){
+            return this.comicsRepository.findByGenreAndCountry(genre, country, pageable);
+        }
+        else if(genre != null){
+            return this.comicsRepository.findByGenre(genre, pageable);
+        }
+        else if(country != null){
+            return this.comicsRepository.findByCountry(country, pageable);
+        }
+        else{
+            return this.comicsRepository.findAll(pageable);
+        }
+
+    }
+
+    @Transactional
+    public Page<Comic> getApproximateSearchComic(String searchQuery, Pageable pageable){
+        return this.comicsRepository.findByTitleContainingIgnoreCase(searchQuery, pageable);
     }
 
 
